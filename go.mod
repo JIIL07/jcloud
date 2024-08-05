4b44299f1c07795d1f10e20a2504c3670bf0b4d3
JIIL <romana9059@gmail.com>
2024-08-05 22:36:06 +0300

    cleaning git tree

--- go.mod
+++ go.mod
@@ -10,39 +10,32 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/spf13/cobra v1.8.0
+	github.com/spf13/viper v1.19.0
 )
 
 require (
-<<<<<<< e11e69cc92d7c5a0aef5e57c3c44bea1b6154e12
-	github.com/hashicorp/hcl v1.0.0 // indirect
-=======
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
->>>>>>> Big file structure update, dockerfile does not work currently, some new features in code: new server system (not complete) and new logger
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
-	github.com/sagikazarmark/locafero v0.4.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-<<<<<<< e11e69cc92d7c5a0aef5e57c3c44bea1b6154e12
-=======
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
 
 require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
->>>>>>> Big file structure update, dockerfile does not work currently, some new features in code: new server system (not complete) and new logger
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.18.2
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,80 +1,3 @@
-<<<<<<< e11e69cc92d7c5a0aef5e57c3c44bea1b6154e12:cmd/root.go
-package cmd
-
-import (
-	"encoding/base64"
-	"fmt"
-	"log"
-	"os"
-
-	cloud "github.com/JIIL07/cloudFiles-manager/client"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-)
-
-var ctx *cloud.FileContext
-
-var RootCmd = &cobra.Command{
-	Use:   `cloud`,
-	Short: `Cloud is a cloud file manager CLI`,
-	Long: `Cloud is a cloud file manager CLI that provides various commands to manage files in the cloud.
-It supports commands like init to initialize the cloud, add to add files, and exit to exit the CLI.`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		if cmd.Name() != "login" {
-			if _, err := os.Stat(fmt.Sprintf("%s/.cloud_init_config.json", os.Getenv("TEMP"))); err != nil {
-				log.Println("Use cloud login to create cloud configuration")
-			}
-		}
-	},
-}
-
-func init() {
-	RootCmd.PersistentFlags().BoolP("help", "h", false, "Help")
-
-	viper.SetConfigName(".cloud_init_config")
-	viper.SetConfigType("json")
-	viper.AddConfigPath(os.Getenv("TEMP"))
-
-	cloud.Generator(32)
-}
-
-func saveConfig() {
-	if err := viper.WriteConfigAs(fmt.Sprintf("%s/.cloud_init_config.json", os.Getenv("TEMP"))); err != nil {
-		log.Println("Error writing config file:", err)
-	}
-}
-
-func loadUserPass() {
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			log.Println("Error reading config file:", err)
-		}
-	} else {
-		username := viper.GetString("username")
-		password := viper.GetString("password")
-
-		decodedUsername, err := base64.StdEncoding.DecodeString(username)
-		if err != nil {
-			log.Println("Error decoding username:", err)
-		}
-		decodedPassword, err := base64.StdEncoding.DecodeString(password)
-		if err != nil {
-			log.Println("Error decoding password:", err)
-		}
-
-		userByte, err := cloud.Decrypt(decodedUsername)
-		if err != nil {
-			log.Println("Error decrypting username:", err)
-		}
-		passByte, err := cloud.Decrypt(decodedPassword)
-		if err != nil {
-			log.Println("Error decrypting password:", err)
-		}
-		fmt.Println("Username:", string(userByte))
-		fmt.Println("Password:", string(passByte))
-	}
-}
-=======
 package cmd
 
 import (
@@ -106,13 +29,14 @@ It supports commands like init to initialize the cloud, add to add files, and ex
 func init() {
 	RootCmd.PersistentFlags().BoolP("help", "h", false, "Help")
 
-	viper.SetConfigName(".cloudconfig")
+	viper.SetConfigName(".cloud_init_config")
 	viper.SetConfigType("json")
-	viper.AddConfigPath(os.Getenv("TMPDIR"))
+	viper.AddConfigPath(os.Getenv("TEMP"))
+
 }
 
 func saveConfig() {
-	if err := viper.WriteConfigAs(fmt.Sprintf("%s/.cloudconfig.json", os.Getenv("TMPDIR"))); err != nil {
+	if err := viper.WriteConfigAs(fmt.Sprintf("%s/.cloud_init_config.json", os.Getenv("TEMP"))); err != nil {
 		log.Println("Error writing config file:", err)
 	}
 }
@@ -124,8 +48,8 @@ func loadUserPass() {
 		}
 	} else {
 		username := viper.GetString("username")
+		password := viper.GetString("password")
 
-		fmt.Println("Username:", username)
+		_, _ = username, password
 	}
 }
->>>>>>> Big file structure update, dockerfile does not work currently, some new features in code: new server system (not complete) and new logger:internal/cmd/root.go
