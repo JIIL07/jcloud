b2f41b6822b04dfd38a3fb12a1758414e2bb11db
JIIL <romana9059@gmail.com>
2024-08-25 01:13:07 +0300

    del unnecessary commnets

--- internal/client/anchor/anchor.go
+++ internal/client/anchor/anchor.go
@@ -22,34 +22,28 @@ type Anchor struct {
 }
 
 func NewAnchor(files []models.File, message string, previousSnapshots map[int]*delta.Snapshot) (Anchor, error) {
-	// Generate a unique anchor ID
 	anchorID, err := GenerateAnchorID()
 	if err != nil {
 		return Anchor{}, err
 	}
 	timestamp := time.Now()
 
-	// Initialize deltas map and file summaries slice
 	deltas := make(map[int]*delta.Delta)
-	fileSummaries := make([]string, 0, len(files)) // Preallocate to avoid resizing
+	fileSummaries := make([]string, 0, len(files))
 
 	for _, file := range files {
-		// Calculate the hash for the file data
 		hash := sha256.New()
 		hash.Write(file.Serialize())
 		hashSum := hex.EncodeToString(hash.Sum(nil))
 
-		// Create a new snapshot from the file data
 		newSnapshot := delta.NewSnapshot(file.Serialize())
 
 		var deltaInfo string
 
-		// Check for previous snapshots and create a delta if applicable
 		if previous, ok := previousSnapshots[file.ID]; ok {
 
 			if d := newSnapshot.CreateDelta(previous); d != nil {
 				deltas[file.ID] = d
-				// Log the delta details
 				deltaInfo = fmt.Sprintf("Delta created: Original Hash: %s, New Hash: %s",
 					d.OriginalHash, d.NewHash)
 			} else {
@@ -59,18 +53,15 @@ func NewAnchor(files []models.File, message string, previousSnapshots map[int]*d
 			deltaInfo = "No previous snapshot, no delta created."
 		}
 
-		// Collect summary information for the file
 		fileSummary := fmt.Sprintf("File ID: %d, Filename: %s, Hash: %s, Delta Info: %s",
 			file.ID, file.Metadata.Name, hashSum, deltaInfo)
 		fileSummaries = append(fileSummaries, fileSummary)
 
-		// Update the previous snapshot map with the new snapshot
 		previousSnapshots[file.ID] = newSnapshot
 	}
 
 	log := LogSummary(anchorID, message, timestamp, fileSummaries)
 
-	// Return the created anchor
 	return Anchor{
 		ID:        anchorID,
 		Message:   message,
